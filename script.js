59fae2092cebbb49eb1ea049df89302426e71a04
VishaliniGopala <gopalavishalini26@gmail.com>
2026-05-29 01:42:22 +0530

    Implemented add delete and complete task functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value.trim();
+
+    if(taskText === ""){
+        alert("Please enter a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    li.addEventListener("click", function(){
+        li.classList.toggle("completed");
+    });
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function(){
+
+        li.remove();
+
+    });
+
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+});
